6b4bcecb30f6dbc77f8db8fcd95b54b0f083a47b
Nattapong Tonprasert <luciferultram@gmail.com>
2013-10-22 11:14:39 +0700

    Implement sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Ticketee::Application.routes.draw do
+  get "users/new"
+  get "users/create"
+  get "users/show"
 	root "projects#index"
+	
+	resources :users
 	
 	resources :projects do
 		resources :tickets
 	end
+	
+	
 end
