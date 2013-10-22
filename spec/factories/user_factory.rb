152a6d380de96e2a1083853a83e14f3944db9602
Nattapong Tonprasert <luciferultram@gmail.com>
2013-10-23 04:36:03 +0700

    Associate users to tickets when tickets are created

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 	factory :user do
-    	name "username"
-		email "email"
+    	name "ultram"
+		email "sample@example.com"
 		password "hunter2"
 		password_confirmation "hunter2"
 	end
